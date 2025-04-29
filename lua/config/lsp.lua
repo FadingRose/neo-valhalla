7ccf8581b4755784350bb3121d9501788ced60db
小早川優 <kobayakawayuu@kobayakawayuus-Mac-mini.local>
2025-04-29 12:18:21 +0800

    lsp: remove manually setup, use Mason for Solidity

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,15 +1,15 @@
-local lspconfig = require("lspconfig")
-local configs = require("lspconfig.configs")
+-- local lspconfig = require("lspconfig")
+-- local configs = require("lspconfig.configs")
 
 -- manully introduce solidity-language-server
 -- npm install @nomicfoundation/solidity-language-server -g
-configs.solidity = {
-  default_config = {
-    cmd = { "nomicfoundation-solidity-language-server", "--stdio" },
-    filetypes = { "solidity" },
-    root_dir = lspconfig.util.find_git_ancestor,
-    single_file_support = true,
-  },
-}
-
-lspconfig.solidity.setup({})
+-- configs.solidity = {
+--   default_config = {
+--     cmd = { "nomicfoundation-solidity-language-server", "--stdio" },
+--     filetypes = { "solidity" },
+--     root_dir = lspconfig.util.find_git_ancestor,
+--     single_file_support = true,
+--   },
+-- }
+--
+-- lspconfig.solidity.setup({})
