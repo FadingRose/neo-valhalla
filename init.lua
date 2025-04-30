d4afbb7071cbc8e63753e1a8ee68cf5ac2cd1120
kobayakawaami <kobayakawaami@gmail.com>
2025-04-30 10:52:04 +0800

    add: better 'w' and 'b'
    now 'w' will NOT jump to next line, 'w' and 'b' will stay at a fixed
    line.

--- init.lua
+++ init.lua
@@ -4,3 +4,27 @@ require("config.lazy")
 vim.opt.signcolumn = "yes"
 
 require("config.lsp")
+
+-- now the w and b only move cursor at the same line
+vim.cmd([[
+  function! CustomW()
+    let start_line = line('.')
+    normal! w
+    if line('.') != start_line
+      execute start_line . 'normal! $'
+    endif
+  endfunction
+
+  function! CustomB()
+    let start_line = line('.')
+    normal! b
+    if line('.') != start_line
+      execute start_line . 'normal! ^'
+    endif
+  endfunction
+
+  nnoremap <silent> w :call CustomW()<CR>
+  nnoremap <silent> b :call CustomB()<CR>
+  vnoremap <silent> w :call CustomW()<CR>
+  vnoremap <silent> b :call CustomB()<CR>
+]])
